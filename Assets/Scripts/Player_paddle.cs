93421a41efa991cb6e2ee71738dd2a3665a801cf
ykupas <y.kupas.7@gmail.com>
2022-05-02 21:54:20 -0300

    Implemented, missing walls collision to stop paddle to go out of screen.

--- Assets/Scripts/Player_paddle.cs
+++ Assets/Scripts/Player_paddle.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Player_paddle : paddle
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Direction variable for player input
+    private Vector2 _direction;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Check what key was pressed
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            _direction = Vector2.up;
+        }
+        else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            _direction = Vector2.down;
+        }
+        else
+        {
+            _direction = Vector2.zero;
+        }
+    }
+
+    // Fixed update is called at a fixed timestamp
+    private void FixedUpdate() 
+    {
+        if(_direction.sqrMagnitude != 0)
+        {
+            _rigidbody.AddForce(_direction * _speed);
+        }
     }
 }
--- Assets/Scripts/paddle.cs
+++ Assets/Scripts/paddle.cs
@@ -4,6 +4,9 @@ public class paddle : MonoBehaviour
 {
     // Protected variable can be accessed from child classes
     protected Rigidbody2D _rigidbody;
+    
+    // Paddle speed variable
+    public float _speed = 10.0f;
 
     // Awake is called one time, for initialization
     private void Awake() 
