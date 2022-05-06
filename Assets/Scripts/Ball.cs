0a2039f67c1fa33c9c41460da961a91aa925bfbf
ykupas <y.kupas.7@gmail.com>
2022-05-05 21:26:27 -0300

    Adding starting force to ball.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Private variable for Rigidbody2D
+    private Rigidbody2D _rigidbody;
+    // Speed variable
+    private float _speed = 200.0f;
+
+    // Awake is called before the first frame update
+    private void Awake()
     {
-        
+        // Point Ball rigidbody to variable
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called just after Awake
+    private void Start() 
     {
-        
+        AddStartingForce();    
     }
+
+    // Adding a starting force to ball
+    private void AddStartingForce()
+    {
+        // Variables for X and Y axis force
+        float x;
+        float y;
+
+        // Create a random starting X direction
+        if(Random.value < 0.5f)
+            x = -1f;
+        else
+            x = +1f;
+
+        // Create a random starting Y direction
+        if(Random.value < 0.5f)
+            // If maximum was 0f, ball can go totally horizontal
+            y = Random.Range(-1.0f, -0.5f);
+        else
+            // If minimum was 0f, ball can go totally horizontal
+            y = Random.Range(0.5f, 1.0f);
+
+        // Add starting force to Ball's Rigidbody2D
+        Vector2 direction = new Vector2(x, y);
+        _rigidbody.AddForce(direction * _speed);
+    }
+
 }
