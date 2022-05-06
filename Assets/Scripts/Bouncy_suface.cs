819f5c6b2673e305827c681c780dce96a7e5a699
ykupas <y.kupas.7@gmail.com>
2022-05-06 00:15:59 -0300

    Creating bouncy surface script.

---
+++ Assets/Scripts/Bouncy_suface.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bouncy_suface : MonoBehaviour
+{
+    // Bounce Strength variable
+    public float bounceStrength;
+
+    // This is called every time a collision 2D is detected
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        
+    }
+}
