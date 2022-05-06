3a0de8bfe3a84d324ad5eb0bd24c59a2ac9325c9
Ygor Kupas <y.kupas.7@gmail.com>
2022-05-05 21:32:00 -0300

    Feature/ball_movement (#10)
    * Adding a ball script.
    
    * Adding starting force to ball.
    
    * Add Ball's bounce just by adding a Physics Material 2D with Bounce 1 to Ball.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Private variable for Rigidbody2D
+    private Rigidbody2D _rigidbody;
+    // Speed variable
+    private float _speed = 200.0f;
+
+    // Awake is called before the first frame update
+    private void Awake()
+    {
+        // Point Ball rigidbody to variable
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called just after Awake
+    private void Start() 
+    {
+        AddStartingForce();    
+    }
+
+    // Adding a starting force to ball
+    private void AddStartingForce()
+    {
+        // Variables for X and Y axis force
+        float x;
+        float y;
+
+        // Create a random starting X direction
+        if(Random.value < 0.5f)
+            x = -1f;
+        else
+            x = +1f;
+
+        // Create a random starting Y direction
+        if(Random.value < 0.5f)
+            // If maximum was 0f, ball can go totally horizontal
+            y = Random.Range(-1.0f, -0.5f);
+        else
+            // If minimum was 0f, ball can go totally horizontal
+            y = Random.Range(0.5f, 1.0f);
+
+        // Add starting force to Ball's Rigidbody2D
+        Vector2 direction = new Vector2(x, y);
+        _rigidbody.AddForce(direction * _speed);
+    }
+
+}
