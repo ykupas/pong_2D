ea909c14352d873cab3f317bf3d5714849f8f884
ykupas <y.kupas.7@gmail.com>
2022-05-06 00:41:27 -0300

    Creating game manager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    // Score variables
+    private int _playerScore;
+    private int _computerScore;
+
+    // Ball reference
+    public Ball ball;
+
+    // Adding player score
+    public void AddPlayerScore()
+    {
+        // Add score
+        _playerScore++;
+        // Reset ball 
+        this.ball.ResetPosition();
+    }
+
+    // Adding Computer score
+    public void AddComputerScore()
+    {
+        // Add score
+        _computerScore++;
+        // Reset ball 
+        this.ball.ResetPosition();
+    }
+
 }
